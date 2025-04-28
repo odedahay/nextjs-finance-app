178e78e23f06c2223916f3e738a8e60e2611812a
odedahay <odedahay@yahoo.com>
2025-04-28 05:41:30 -0600

    Update dependencies and refactor styles: added 'lucide-react' package, modified 'globals.css' for Tailwind CSS utility classes, and simplified 'page.js' component structure.

---
+++ app/playground/page.jsx
@@ -0,0 +1,28 @@
+import PageHeader from "@/components/page-header";
+import Trend from "./trend";
+
+export default function Page() {
+    return (
+        <main className="space-y-8">
+            <h1 className="text-4xl mt-8">Playground</h1>
+
+            <div>
+                <h2 className="mb-4 text-lg front-mono">PageHeader</h2>
+                <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+                <div><PageHeader /></div>
+            </div>
+
+            <div>
+                <h2 className="mb-4 text-lg front-mono">Trend</h2>
+                <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+                <div className="flex space-x-4">
+                    <Trend type="Income" amount={1000} prevAmount={900} />
+                    <Trend type="Expense" amount={12000} prevAmount={10000} />
+                    <Trend type="Investment" amount={7000} prevAmount={11100} />
+                    <Trend type="Saving" amount={500} prevAmount={950} />
+                </div>
+            </div>
+
+        </main>
+    )
+}
